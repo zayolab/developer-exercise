07ab7831352a19c98ca9ca595a626de9c8a634ed
Brandon Verkamp <jadelclemens@gmail.com>
2019-04-13 20:45:42 -0600

    Check argument type in capitalize

--- src/helper.js
+++ src/helper.js
@@ -4,5 +4,7 @@
  * Capitalizes a string, i.e. uppercases the first letter
  */
 export function capitalize(str) {
+    //check for invalid (nonexistant or empty) string
+    if(!str) throw new Error("String must not be empty");
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
