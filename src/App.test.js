b5cba215c8e03c6ff62f16349464d60d9d2542dc
Brandon Verkamp <jadelclemens@gmail.com>
2019-04-13 20:46:04 -0600

    Add app tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {ledger} from "./ledger.js";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('ledgerAt(index) returns the correct ledgers', () => {
+    const app = new App();
+    const submit = app.findByProps({})
+    expect(app.ledgerAt(0)).toBe(app.ledgers.revenue[0]);
+    expect(app.ledgerAt(1)).toBe(app.ledgers.expenses[0]);
+})
+
