f32a3c02fa1184b4f8bb2d899ec73b195f3191f7
Josh <grafton.josh@gmail.com>
2019-04-17 10:29:42 -0600

    refactored addDataForm template

--- src/components/AddDataForm.js
+++ src/components/AddDataForm.js
@@ -7,55 +7,50 @@ import {
  } from 'react-bootstrap'
 
 export const AddDataForm = props => {
+const initialFormState = { id: null, name: '', username: '' }
+const [user, setUser] = useState(initialFormState)
+
   return (
-    <Form className="addExpenseOrRevenueForm" >
-      <Row className="input-field">
-        <Col sm={{ span: 2, offset: 1}} className="input-field">
-          <Form.Control
-            as="select"
-            onChange = ''
-            value= ''
-            >
-            <option value="choose">Select Type</option>
-            <option value="revenue">Revenue</option>
-            <option value="expenses">Expense</option>
-          </Form.Control>
-        </Col>
-        <Col sm={3} className="input-field">
-          <Form.Control
-            type="text"
-            placeholder="Name"
-            onChange = ''
-            value=''
-          />
-        </Col>
-        <Col sm={2} className="input-field">
-          <Form.Control
-            type="number"
-            placeholder="One-Time Amount"
-            onChange = ''
-            step="0.01"
-            min="0"
-            value=''
-          />
-        </Col>
-        <Col sm={2} className="input-field">
-          <Form.Control
-            type="number"
-            placeholder="Monthly Amount"
-            onChange = ''
-            step="0.01"
-            min="0"
-            value=''
-          />
-        </Col>
-        <Col sm={1} className="add-form-button">
-          <Button type="submit">
-            Add
-          </Button>
-        </Col>
-      </Row>
-    </Form>
+      <Form>
+        <Row className="input-field">
+          <Col sm={{ span: 2, offset: 1}} className="input-field">
+            <Form.Control as="select">
+              <option>Revenue</option>
+              <option>Expense</option>
+            </Form.Control>
+          </Col>
+          <Col sm={3} className="input-field">
+            <Form.Control
+              type="text"
+              placeholder="Name"
+              value=''
+            />
+          </Col>
+          <Col sm={2} className="input-field">
+            <Form.Control
+              type="number"
+              placeholder="One-Time Amount"
+              step="0.01"
+              min="0"
+              value=''
+            />
+          </Col>
+          <Col sm={2} className="input-field">
+            <Form.Control
+              type="number"
+              placeholder="Monthly Amount"
+              step="0.01"
+              min="0"
+              value=''
+            />
+          </Col>
+          <Col sm={1} className="add-form-button">
+            <Button type="submit">
+              Add
+            </Button>
+          </Col>
+        </Row>
+      </Form>
   )
 }
 
