492c67881af64471ece86f3d1431bb2ab4d21949
willdavidking <will.david.king@gmail.com>
2019-04-21 19:44:51 -0600

    refactor/individual-forms - Removed Add Expense or Revenue form from within render of App, putting into separate file.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import {
-  Row,
-  Col,
   Button,
-  Form
  } from 'react-bootstrap';
 import {ChooseTermLength} from './ChooseTermLength';
 import {DisplayRoiTable} from './DisplayRoiTable';
+import {AddNewItemForm} from './AddNewItemForm';
 import './App.css';
 
 class App extends Component {
@@ -198,58 +196,18 @@ class App extends Component {
           onTermChange={this.handleTermChange}
           />
         {/* Add new expense or revenue form */}
-        <Form className="addExpenseOrRevenueForm" onSubmit={this.handleAdd}>
-          <Row className="input-field">
-            <Col sm={{ span: 2, offset: 1}} className="input-field">
-              <Form.Control
-                as="select"
-                onChange = {this.handleTypeChange}
-                value={this.state.newType ? this.state.newType : 'choose'}
-                >
-                <option value="choose" disabled={true}>Select Type</option>
-                <option value="revenue">Revenue</option>
-                <option value="expenses">Expense</option>
-              </Form.Control>
-            </Col>
-            <Col sm={3} className="input-field">
-              <Form.Control
-                type="text"
-                placeholder="Name"
-                onChange = {this.handleNameChange}
-                value={this.state.newName ? this.state.newName : ''}
-              />
-            </Col>
-            <Col sm={2} className="input-field">
-              <Form.Control
-                type="number"
-                placeholder="One-Time Amount"
-                onChange = {this.handleOneTimeChange}
-                step="0.01"
-                min="0"
-                value={(this.state.newOneTime || this.state.newOneTime === 0) ? this.state.newOneTime : ''}
-              />
-            </Col>
-            <Col sm={2} className="input-field">
-              <Form.Control
-                type="number"
-                placeholder="Monthly Amount"
-                onChange = {this.handleMonthlyChange}
-                step="0.01"
-                min="0"
-                value={(this.state.newMonthly || this.state.newMonthly === 0) ? this.state.newMonthly : ''}
-              />
-            </Col>
-            <Col sm={1} className="add-form-button">
-              <Button type="submit">
-                Add
-              </Button>
-            </Col>
-          </Row>
-        </Form>
+        <AddNewItemForm
+          onTypeChange={this.handleTypeChange}
+          onNameChange={this.handleNameChange}
+          onOneTimeChange={this.handleOneTimeChange}
+          onMonthlyChange={this.handleMonthlyChange}
+          onHandleAdd={this.handleAdd}
+        />  
         {/* form errors */}
         { this.state.error &&
           <h4 className="error text-center">Please fill out all fields</h4>
         }
+        {/* Display Return of Investment Tables */}
         <DisplayRoiTable 
           /* Revenue Table Data */
           revenueData={revenueTableData}
