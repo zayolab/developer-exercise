1a1d7abd592746111a901363b4bf8ddcae311a4d
willdavidking <will.david.king@gmail.com>
2019-04-22 21:11:13 -0600

    refactor/moving-value-calculator - Added delete buttons back to Expenses and Revenue tables.

--- src/getExpensesTableData.js
+++ src/getExpensesTableData.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import {Button} from 'react-bootstrap' 
 import './App.css'
 
-export const getExpensesTableData = (expenses) => {
+export const getExpensesTableData = (expenses, onDelete) => {
   return (
-    expenses.map((expense, index) => {
+    expenses.map((expenses, index) => {
       return (
         <tr key={"expense" + index}>
-          <td>{expense.name}</td>
-          <td>${expense.oneTime.toFixed(2)}</td>
-          <td>${expense.monthly.toFixed(2)}</td>
+          <td>{expenses.name}</td>
+          <td>${expenses.oneTime.toFixed(2)}</td>
+          <td>${expenses.monthly.toFixed(2)}</td>
+          <td><Button onClick={() => onDelete('expenses', index)}>Delete</Button></td>
         </tr>
       )
     })
