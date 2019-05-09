38303685f5c794b43522841eff3186a6d5182a03
okabe-san <aokabe@developers.wyyerd.com>
2019-05-09 09:46:25 -0600

    delete total cal from main

--- src/App.js
+++ src/App.js
@@ -146,16 +146,6 @@ class App extends Component {
   }
 
   render() {
-    // Calculations for totals
-    let totalRevenue = this.state.oneTimeRevenue + (this.state.monthlyRevenue * 12)
-    let totalExpense = this.state.oneTimeExpense + (this.state.monthlyExpense * 12)
-    let monthlyContributionProfit = this.state.monthlyRevenue - this.state.monthlyExpense
-    let totalContributionProfit = totalRevenue - totalExpense
-    // handle case where totalRevenue is 0 (to avoid -Infinity and NaN)
-    let contributionMargin = totalRevenue !== 0 ? (totalContributionProfit / totalRevenue * 100).toFixed(0) : 0
-    // handle case where totalExpense and totalRevenue are 0 (to avoid NaN)
-    let capitalROI = (totalExpense === 0 && totalRevenue === 0) ? 0 : ((this.state.oneTimeExpense - this.state.oneTimeRevenue) / monthlyContributionProfit).toFixed(1)
-
     return (
       <div>
         <h1 className="text-center">ROI Calculator</h1>
--- src/calculations.js
+++ src/calculations.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {} from 'react';
 
 class Calculation extends React.Component {
   constructor(props) {
