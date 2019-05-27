fe3320798be087d12f6a7b448ba33a9a3e1ec72b
gaborszekely <gabor_szekely@hotmail.com>
2019-05-26 22:49:39 -0600

    Wrote unit tests for App

--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -1,8 +1,29 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { mount } from "../../enzyme"
 import App from "./index"
 
-it("renders without crashing", () => {
-  const div = document.createElement("div")
-  ReactDOM.render(<App />, div)
+describe("App tests", () => {
+  let wrapper
+  const handleDelete = jest.fn()
+  const handleUpdate = jest.fn()
+
+  beforeEach(() => {
+    const timePeriod = 6
+
+    wrapper = mount(<App />)
+  })
+
+  it("renders without crashing", () => {
+    const div = document.createElement("div")
+    ReactDOM.render(<App />, div)
+  })
+
+  it("renders the AddTransaction Component", () => {
+    expect(wrapper.find("AddTransaction")).toBeDefined()
+  })
+
+  it("renders the SetTimePeriod Component", () => {
+    expect(wrapper.find("SetTimePeriod")).toBeDefined()
+  })
 })
